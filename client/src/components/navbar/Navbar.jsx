05ad39b713074380d87e44265cd931b2b5cc7e76
mykola-mmm <mykola.mazniuk@gmail.com>
2024-05-01 15:24:05 +0200

    added navbar prototype, testing

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import "./navbar.scss"
+import React, { useState } from "react";
+import "./navbar.scss";
 
 export default function Navbar() {
+  const [open, setOpen] = React.useState(false);
   return (
     <>
-    <nav>
-        <div className='left'>
-          <a href="/" className='logo'>
+      <nav>
+        <div className="left">
+          <a href="/" className="logo">
             <img src="/logo.png" alt="" />
             <span>Agency</span>
           </a>
@@ -15,14 +16,24 @@ export default function Navbar() {
           <a href="">Contacts</a>
           <a href="">Others</a>
         </div>
-        <div className='right'>
+        <div className="right">
           <a href="">Sign In</a>
-          <a href="" className='register'>Sign Up</a>
-          <div className='menuIcon'>
-            <img src="/menu.png" alt="" />
+          <a href="" className="register">
+            Sign Up
+          </a>
+          <div className="menuIcon">
+            <img src="/menu.png" alt="" onClick={() => setOpen(!open)} />
+          </div>
+          <div className={open ? "menu active" : "menu"}>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contacts</a>
+            <a href="">Others</a>
+            <a href="">Sign In</a>
+            <a href="">Sign up</a>
           </div>
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
